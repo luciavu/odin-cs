538eac76b90b807e61484c58aa329d0fafa35d92
Lucia Vu <luciavu6@outlook.com>
2024-11-13 05:25:10 +1000

    Add fibonacci solution

---
+++ recursion/fibonacci.js
@@ -0,0 +1,28 @@
+// Iterative solution
+const fibs = (num) => {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+
+  let fibArray = [0, 1];
+
+  for (let i = 2; i < num; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+  }
+
+  return fibArray;
+};
+
+// Recursive solution
+const fibsRec = (num) => {
+  if (num <= 0) return [];
+  if (num == 1) return [0];
+  if (num == 2) return [0, 1];
+
+  const fibArray = fibsRec(num - 1);
+  const nextFib = fibArray[fibArray.length - 2] + fibArray[fibArray.length - 1];
+  return [...fibArray, nextFib];
+};
+
+num = 8;
+console.log('Fibonacci sequence via iteration:', fibs(num));
+console.log('Fibonacci array via recursion:', fibsRec(num));
